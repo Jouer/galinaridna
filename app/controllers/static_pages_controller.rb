9ceddd0aee10b674d5d3c8cfa3165545b803d95e
vagrant <vagrant@rails-dev-box>
2013-11-15 09:53:21 +0000

    repair erroor show

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -18,7 +18,7 @@ class StaticPagesController < ApplicationController
   	if (@page == Page.where(:permalink => 'black').first)
   		img_set = ['col1.png', 'col2.png', 'col3.png']
   		else
-  	   img_set = ['one.png', 'two.png', 'three.png', 'four.png', 'five.png',   'seven.png', 'eight.png', 'nine.png']
+  	   img_set = ['one.png', 'two.png', 'three.png', 'four.png', 'five.png', 'seven.png', 'eight.png', 'nine.png']
   	    end
   	quantity = img_set.count
   	r = Random.new
@@ -28,35 +28,14 @@ class StaticPagesController < ApplicationController
   
   def picture
   	 
- 	@section = Section.find(params[:id])
+ 	@section = Section.find_by_name(params[:image_name])
  	@page = Page.where(:id => params[:page_id]).first
- 	 @img_name = params[:image_name]
- 	 
-
-   img_set = [] 
-    if @page 
-   @page.sections.each do |section| 
-   if section.name != 'info'
-   a = []
-   a[0]= section.name
-   a[1] = '.png'
-   name = a.join('') 
-    img_set << name 
-	end
- 	end
-	 img_set.find {|i| i==@img_name}
-	  n=img_set.index {|i| i==@img_name} 
-	 	if n < (img_set.count - 1)
- 	 	@next_img=img_set[n+1]
- 	 	else
- 		@next_img=img_set[0]
-  		end 
-    if n > 0 
-   @prev_img=img_set[n-1]
-  	 else 
-  	@prev_img=img_set[-1] 
- 	end
- 	end
+    @img_name = params[:image_name]
+ 	
+   @set_img = @page.sections.order(position: :asc).where.not(name: 'info' )
+   
+   @next_img = @set_img.where(name: @img_name)
+  
  	 
  	end
  	
